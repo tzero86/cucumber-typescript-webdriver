8da4b29a38abf003450d45fb69090c2926d87920
tzero86 <eliasmedina86@gmail.com>
2023-03-20 13:28:04 -0300

    Updated the way we invoke cucumber-js and added babel transpiling.

---
+++ e2e/src/index.ts
@@ -0,0 +1,14 @@
+const common = './src/features/**/*.feature \
+  --require-module ts-node/register \
+  --require ./src/step-definitions/**/*.ts \
+  -f json:./reports/report.json \
+  --format progress-bar '
+
+
+const dev = `${common} --tags '@dev'`
+const smoke = `${common} --tags '@smoke'`
+const regression = `${common} --tags '@regression'`
+
+console.log(`✨✨✨✨✨✨`)
+
+export { dev, smoke, regression}
