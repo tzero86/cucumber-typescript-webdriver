408acc089f8c30e154d15c18108ac261a7df7418
tzero86 <eliasmedina86@gmail.com>
2023-03-20 14:01:02 -0300

    Added common config file .env, added cross-env so it works on Win11.

--- e2e/src/index.ts
+++ e2e/src/index.ts
@@ -1,3 +1,10 @@
+import dotenv from 'dotenv'
+import { env } from './env/parseEnv'
+
+dotenv.config({path: env('COMMON_CONFIG_FILE')})
+
+
+
 const common = './src/features/**/*.feature \
   --require-module ts-node/register \
   --require ./src/step-definitions/**/*.ts \
@@ -9,6 +16,6 @@ const dev = `${common} --tags '@dev'`
 const smoke = `${common} --tags '@smoke'`
 const regression = `${common} --tags '@regression'`
 
-console.log(`✨✨✨✨✨✨`)
+console.log(`✨✨✨MAGIC IS STARTING✨✨✨`)
 
 export { dev, smoke, regression}
