821986365a9e4bd566aaaddce9bb9cca50f5c60c
tzero86 <eliasmedina86@gmail.com>
2023-04-10 19:03:12 -0300

    Force download latest chromedriver onto agent

--- scripts/buildkite-automation.sh
+++ scripts/buildkite-automation.sh
@@ -12,5 +12,16 @@ cd e2e
 echo "yarn install e2e"
 yarn install
 
+echo "install latest chrome driver"
+# Install the latest Chrome and Chromedriver
+apk update
+apk add --no-cache xvfb chromium chromium-chromedriver
+LATEST_CHROMEDRIVER=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget https://chromedriver.storage.googleapis.com/$LATEST_CHROMEDRIVER/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+chmod +x chromedriver
+mv chromedriver /usr/bin
+rm chromedriver_linux64.zip
+
 echo "run e2e tests"
 ./run_tests.sh localhost regression
